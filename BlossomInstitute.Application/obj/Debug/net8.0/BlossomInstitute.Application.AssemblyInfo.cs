12391e08eee15f7d31ce8190ab7a94435463fce1
Ulises <ulipvp@gmail.com>
2026-02-20 15:46:43 -0300

    Desarrollo de DesactivarProfesor Command

--- BlossomInstitute.Application/obj/Debug/net8.0/BlossomInstitute.Application.AssemblyInfo.cs
+++ BlossomInstitute.Application/obj/Debug/net8.0/BlossomInstitute.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlossomInstitute.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9359eb228cda1d747f26c6fed1425f8129cb033b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1904bf2a848d0f8835bec7193afb52fd145664d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlossomInstitute.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlossomInstitute.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
