c7053a28446497f2968403761f5d5f1ab3226ade
Ulises <ulipvp@gmail.com>
2026-03-15 20:50:05 -0300

    Swagger

--- BlossomInstitute/Program.cs
+++ BlossomInstitute/Program.cs
@@ -26,16 +26,13 @@ catch (Exception ex)
     throw;
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blossom Institute v1");
         c.RoutePrefix = string.Empty;
     });
-}
-
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
